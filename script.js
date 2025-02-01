78e5b9d95abe92d8d7bc20f0855a41c4fe92814c
Tailung1 <ramazsamitashvili11@gmail.com>
2025-02-01 22:39:02 +0400

    created one password strength inidcator function and use it everyhere where needed

--- script.js
+++ script.js
@@ -5,109 +5,11 @@ const lengthElement=document.querySelector('#length')
 rangeValue.textContent=lengthElement.value
 const boxes=document.querySelectorAll('.box')
 const passRaiting=document.querySelector('.password-strength')
-
-function strengthCheck(counter) {
-    const strengths=['None','Medium','Good','Very Good','Best'];
-    passRaiting.textContent=strengths[counter]
-}
-
 const copyImg=document.querySelector('.copy')
 const textToCopy=document.querySelector('.password')
 
-
-
-copyImg.addEventListener('click',()=> {
-    navigator.clipboard.writeText(textToCopy.textContent)
-    .then(()=> {
-        alert('Text copied to clipboard')
-    }) .catch(error => {
-        alert('Failed to copy text:'+error)
-    })
-})
-
-
-
-let counter=0;
-const checkboxes=Array.from(document.querySelectorAll('input[type="checkbox"]'))
-
-const uppercaseInclude2=document.querySelector('#uppercase')
-const lowercaseInclude2=document.querySelector('#lowercase');
-const numbersInclude2=document.querySelector('#numbers');
-const symbolsInclude2=document.querySelector('#symbols')
-
-
-uppercaseInclude2.addEventListener('input',()=> {
-    if(uppercaseInclude2.checked) {
-        counter++
-        console.log(counter)
-        for(let i=0;i<counter;i++) {
-            if(boxes[i]) {
-                boxes[i].style.backgroundColor='red'
-            }
-            strengthCheck(counter)      
-        }
-    } else {
-        counter--
-        console.log(counter)
-        for(let i=0;i<=counter;i++) {
-            if(boxes[i]) {
-                boxes[counter].style.backgroundColor='white'
-                
-            }
-            strengthCheck(counter)
-        } 
-    }
-})
-
-lowercaseInclude2.addEventListener('input',()=> {
-    if(lowercaseInclude2.checked) {
-        counter++
-        console.log(counter)
-        for(let i=0;i<counter;i++) {
-            if(boxes[i]) {
-                boxes[i].style.backgroundColor='red'
-            }
-            if(boxes[i]) {
-                boxes[i].style.backgroundColor='red'
-            }
-            strengthCheck(counter)
-        }
-    } else {
-        counter--
-        console.log(counter)
-        for(let i=0;i<=counter;i++) {
-            if(boxes[i]) {
-                boxes[counter].style.backgroundColor='white'             
-            }
-            strengthCheck(counter)
-        }
-    }
-    
-})
-
-numbersInclude2.addEventListener('input',()=> {
-    if(numbersInclude2.checked) {
-        counter++
-        console.log(counter)
-        for(let i=0;i<counter;i++) {
-            if(boxes[i]) {
-                boxes[i].style.backgroundColor='red'
-            }
-            strengthCheck(counter)
-        }
-    } else {
-        counter--
-        console.log(counter)
-        for(let i=0;i<=counter;i++) {
-            if(boxes[i]) {
-                boxes[counter].style.backgroundColor='white'        
-            }
-            strengthCheck(counter)
-        }
-    }
-})
-symbolsInclude2.addEventListener('input',()=> {
-    if(symbolsInclude2.checked) {
+function strengthLights(option) {
+    if(option.checked) {
         counter++
         console.log(counter)
         for(let i=0;i<counter;i++) {
@@ -126,8 +28,48 @@ symbolsInclude2.addEventListener('input',()=> {
             strengthCheck(counter)     
         }
     }
+}
+
+function strengthCheck(counter) {
+    const strengths=['None','Medium','Good','Very Good','Best'];
+    passRaiting.textContent=strengths[counter]
+}
+
+
+copyImg.addEventListener('click',()=> {
+    navigator.clipboard.writeText(textToCopy.textContent)
+    .then(()=> {
+        alert('Text copied to clipboard')
+    }) .catch(error => {
+        alert('Failed to copy text:'+error)
+    })
 })
 
+
+let counter=0;
+const checkboxes=Array.from(document.querySelectorAll('input[type="checkbox"]'))
+
+const uppercaseInclude2=document.querySelector('#uppercase')
+const lowercaseInclude2=document.querySelector('#lowercase');
+const numbersInclude2=document.querySelector('#numbers');
+const symbolsInclude2=document.querySelector('#symbols')
+
+
+uppercaseInclude2.addEventListener('input',()=> {
+    strengthLights(uppercaseInclude2)
+})
+
+lowercaseInclude2.addEventListener('input',()=> {
+    strengthLights(lowercaseInclude2)
+    
+})
+
+numbersInclude2.addEventListener('input',()=> {
+    strengthLights(numbersInclude2)
+})
+symbolsInclude2.addEventListener('input',()=> {
+    strengthLights(symbolsInclude2)
+})
 
 
 lengthElement.addEventListener('input',()=> {
