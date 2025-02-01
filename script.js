f5cd44afbcc2aec5195efbaf1c05d7fab3a12e4a
Tailung1 <ramazsamitashvili11@gmail.com>
2025-02-01 04:35:12 +0400

    add many thing.add copy function,add password strentgh indicator

--- script.js
+++ script.js
@@ -3,6 +3,128 @@ const reset=document.querySelector('.resetBtn')
 const rangeValue=document.querySelector('.range-value')
 const lengthElement=document.querySelector('#length')
 rangeValue.textContent=lengthElement.value
+const boxes=document.querySelectorAll('.box')
+
+
+const copyImg=document.querySelector('.copy')
+const textToCopy=document.querySelector('.password')
+
+
+
+copyImg.addEventListener('click',()=> {
+    navigator.clipboard.writeText(textToCopy.textContent)
+    .then(()=> {
+        alert('Text copied to clipboard')
+
+    }) .catch(error => {
+        alert('Failed to copy text:'+error)
+    })
+})
+
+// 2nd way to to copy function
+
+// copyImg.addEventListener('click',()=> {
+//     const tempTextArea=document.createElement('textarea');
+//     tempTextArea.value=textToCopy.textContent;
+//     document.body.appendChild(tempTextArea);
+//     tempTextArea.select();
+//     document.execCommand('copy')
+//     document.body.removeChild(tempTextArea)
+
+//     alert('Text copied to clipboard!')
+// })
+
+
+let counter=0;
+const checkboxes=Array.from(document.querySelectorAll('input[type="checkbox"]'))
+
+const uppercaseInclude2=document.querySelector('#uppercase')
+const lowercaseInclude2=document.querySelector('#lowercase');
+const numbersInclude2=document.querySelector('#numbers');
+const symbolsInclude2=document.querySelector('#symbols')
+
+
+uppercaseInclude2.addEventListener('input',()=> {
+    if(uppercaseInclude2.checked) {
+        counter++
+        console.log(counter)
+    } else {
+        counter--
+        console.log(counter)
+        for(let i=0;i<=counter;i++) {
+            if(boxes[i]) {
+                boxes[counter].style.backgroundColor='white'
+                
+            }
+        } 
+    }
+    for(let i=0;i<counter;i++) {
+        if(boxes[i]) {
+            boxes[i].style.backgroundColor='red'
+        }
+    }
+})
+
+lowercaseInclude2.addEventListener('input',()=> {
+    if(lowercaseInclude2.checked) {
+        counter++
+        console.log(counter)
+    } else {
+        counter--
+        console.log(counter)
+        for(let i=0;i<=counter;i++) {
+            if(boxes[i]) {
+                boxes[counter].style.backgroundColor='white'
+                
+            }
+        }
+    }
+    for(let i=0;i<counter;i++) {
+        if(boxes[i]) {
+            boxes[i].style.backgroundColor='red'
+        }
+    }
+})
+
+numbersInclude2.addEventListener('input',()=> {
+    if(numbersInclude2.checked) {
+        counter++
+        console.log(counter)
+    } else {
+        counter--
+        console.log(counter)
+        for(let i=0;i<=counter;i++) {
+            if(boxes[i]) {
+                boxes[counter].style.backgroundColor='white'        
+            }
+        }
+    }
+    for(let i=0;i<counter;i++) {
+        if(boxes[i]) {
+            boxes[i].style.backgroundColor='red'
+        }
+    }
+})
+symbolsInclude2.addEventListener('input',()=> {
+    if(symbolsInclude2.checked) {
+        counter++
+        console.log(counter)
+    } else {
+        counter--
+        console.log(counter)
+        for(let i=0;i<=counter;i++) {
+            if(boxes[i]) {
+                boxes[counter].style.backgroundColor='white'        
+            }
+        }
+    }
+    for(let i=0;i<counter;i++) {
+        if(boxes[i]) {
+            boxes[i].style.backgroundColor='red'
+        }
+    }
+})
+
 
 
 lengthElement.addEventListener('input',()=> {
@@ -29,6 +151,7 @@ submitBtn.addEventListener('click',()=> {
 
     let characters="";
 
+
     if(uppercaseInclude) characters+=uppercaseChars
     if(lowercaseInclude) characters+=lowercaseChars
     if(numbersInclude) characters+=numberChars
@@ -42,11 +165,18 @@ submitBtn.addEventListener('click',()=> {
     reset.addEventListener('click',()=> {
         document.querySelector('.password').textContent='Generated Password';
         reset.style.display='none'
+        rangeValue.textContent=11;
+        rangeValue.style.color ='#A4FFAF';
+        lengthElement.value=11
         
         document.querySelector('#uppercase').checked=false;
         document.querySelector('#lowercase').checked = false;
         document.querySelector('#numbers').checked = false;
         document.querySelector('#symbols').checked = false;
+
+        for(let i=0;i<=4;i++) {
+            boxes[i].style.backgroundColor='white' 
+        }
     })
 
     let password="";
